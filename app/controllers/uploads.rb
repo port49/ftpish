5fa5d880da5f7e8634d94ed36033e13b39b62a14
CLR <clr@port49.com>
2008-10-29 17:32:22 -0400

    -- upload one or multiple files --

--- app/controllers/uploads.rb
+++ app/controllers/uploads.rb
@@ -5,9 +5,17 @@ class Uploads < Application
   end
   
   def post
-    new_file_path = Merb.root / RAILS_PATH / 'public' / 'files' / params[:directory][:path] / sanitize_filename( params[:file][:filename] )
-    FileUtils.mv params[:file][:tempfile].path, new_file_path
-    File.chmod( 0660, new_file_path )
+    if params[:file].is_a?( Array )
+      params[:file].each |file|
+        new_file_path = Merb.root / RAILS_PATH / 'public' / 'files' / params[:directory][:path] / sanitize_filename( file[:filename] )
+        FileUtils.mv file[:tempfile].path, new_file_path
+        File.chmod( 0664, new_file_path )
+      end
+    else
+      new_file_path = Merb.root / RAILS_PATH / 'public' / 'files' / params[:directory][:path] / sanitize_filename( params[:file][:filename] )
+      FileUtils.mv params[:file][:tempfile].path, new_file_path
+      File.chmod( 0664, new_file_path )
+    end
     redirect "#{ RAILS_URL }/directory?id=#{ params[:directory][:path] }"
   end
 
