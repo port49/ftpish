afdcd2c7667c150d580d5527b9f826008489d0c1
CLR <clr@port49.com>
2008-10-30 10:59:03 -0400

    -- upload one or multiple files: debugging --

--- app/controllers/uploads.rb
+++ app/controllers/uploads.rb
@@ -5,11 +5,10 @@ class Uploads < Application
   end
   
   def post
-raise params.inspect
-    params[:file].each do |file|
-      if file[:filename].length > 0
-        new_file_path = Merb.root / RAILS_PATH / 'public' / 'files' / params[:directory][:path] / sanitize_filename( file[:filename] )
-        FileUtils.mv file[:tempfile].path, new_file_path
+    params[:file].each do |file| # file won't have symbolized keys
+      if file['filename'].length > 0
+        new_file_path = Merb.root / RAILS_PATH / 'public' / 'files' / params[:directory][:path] / sanitize_filename( file['filename'] )
+        FileUtils.mv file['tempfile'].path, new_file_path
         File.chmod( 0664, new_file_path )
       end
     end
